8e802fd462133522d6902b90bb64ca1d733044f6
Luke Imhoff <luke_imhoff@rapid7.com>
2013-07-05 20:41:57 -0500

    IpFormatValidator specs
    [#52934727]
    [#52833605]

--- app/validators/ip_format_validator.rb
+++ app/validators/ip_format_validator.rb
@@ -1,22 +1,31 @@
-require "ipaddr"
+require 'ipaddr'
 
 # Validates that value is an IPv4 or IPv6 address.
 class IpFormatValidator < ActiveModel::EachValidator
-  # Validates that `value` is an IPv4 or IPv4 address.  Error is `'must be a valid IPv4 or IPv6 address'`.
+  # Validates that `value` is an IPv4 or IPv4 address.  Ranges in CIDR or netmask notation are not allowed.
   #
-  # @param object [#errors, ActiveRecord::Base] ActiveModel or ActiveRecord
+  # @param record [#errors, ActiveRecord::Base] ActiveModel or ActiveRecord
   # @param attribute [Symbol] name of IP address attribute.
   # @param value [String, nil] IP address.
   # @return [void]
   # @see IPAddr#ipv4?
   # @see IPAddr#ipv6?
-  def validate_each(object, attribute, value)
-    error_message_block = lambda{ object.errors[attribute] << "must be a valid IPv4 or IPv6 address" }
+  def validate_each(record, attribute, value)
     begin
       potential_ip = IPAddr.new(value)
-      error_message_block.call unless potential_ip.ipv4? || potential_ip.ipv6?
     rescue ArgumentError
-      error_message_block.call
+      record.errors[attribute] << 'must be a valid IPv4 or IPv6 address'
+    else
+      # if it includes a netmask, then it's not an IP address, but an IP range.
+      if potential_ip.ipv4?
+        if potential_ip.instance_variable_get(:@mask_addr) != IPAddr::IN4MASK
+          record.errors[attribute] << 'must be a valid IPv4 or IPv6 address and not an IPv4 address range in CIDR or netmask notation'
+        end
+      elsif potential_ip.ipv6?
+        if potential_ip.instance_variable_get(:@mask_addr) != IPAddr::IN6MASK
+          record.errors[attribute] << 'must be a valid IPv4 or IPv6 address and not an IPv6 address range in CIDR or netmask notation'
+        end
+      end
     end
   end
 end
