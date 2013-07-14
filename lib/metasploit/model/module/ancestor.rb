470cb4f6b105241cdd47a99fb7009d25a23ead1f
Luke Imhoff <luke_imhoff@rapid7.com>
2013-07-13 22:09:44 -0500

    Share DIRECTORY_BY_MODULE_TYPE
    [#50752269]
    
    Put DIRECTORY_BY_MODULE_TYPE in metasploit-model so it can be deduped
    from metasploit_data_models and metasploit-framework.

--- lib/metasploit/model/module/ancestor.rb
+++ lib/metasploit/model/module/ancestor.rb
@@ -7,15 +7,20 @@ module Metasploit
         # CONSTANTS
         #
 
-        # Module types
-        MODULE_TYPES = [
-            'auxiliary',
-            'encoder',
-            'exploit',
-            'nop',
-            'payload',
-            'post'
-        ]
+        # The directory for a given #module_type is a not always the pluralization of #module_type, so this maps the
+        # #module_type to the type directory that is used to generate the #real_path from the #module_type and
+        # #reference_name.
+        DIRECTORY_BY_MODULE_TYPE = {
+            'auxiliary' => 'auxiliary',
+            'encoder' => 'encoders',
+            'exploit' => 'exploits',
+            'nop' => 'nops',
+            'payload' => 'payloads',
+            'post' => 'post'
+        }
+
+        # The modules types from {DIRECTORY_BY_MODULE_TYPE}.
+        MODULE_TYPES = DIRECTORY_BY_MODULE_TYPE.keys.sort
       end
     end
   end
--- spec/lib/metasploit/model/module/ancestor_spec.rb
+++ spec/lib/metasploit/model/module/ancestor_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 describe Metasploit::Model::Module::Ancestor do
   context 'CONSTANTS' do
+    context 'DIRECTORY_BY_MODULE_TYPE' do
+      subject(:directory_by_module_type) do
+        described_class::DIRECTORY_BY_MODULE_TYPE
+      end
+
+      its(['auxiliary']) { should == 'auxiliary' }
+      its(['encoder']) { should == 'encoders' }
+      its(['exploit']) { should == 'exploits' }
+      its(['nop']) { should == 'nops' }
+      its(['payload']) { should == 'payloads' }
+      its(['post']) { should == 'post' }
+    end
+
     context 'MODULE_TYPES' do
       subject(:module_types) do
         described_class::MODULE_TYPES
