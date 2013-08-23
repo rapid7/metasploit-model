79805ea9275f5eaa3bacf840df62aa326047997e
Luke Imhoff <luke_imhoff@rapid7.com>
2013-08-23 13:44:53 -0500

    lib, app/models, and app/validators in autoload_paths
    [#55226210]
    
    Had to rename app/model to app/models since app/model was a typo.

---
+++ lib/metasploit/model/autoload.rb
@@ -0,0 +1,69 @@
+module Metasploit
+  module Model
+    # Defines methods for adding paths to `ActiveSupport::Dependencies.autoload_paths`.
+    module Autoload
+      # Combines {#autoload_once_paths} and {#autoload_paths} as both need to be added to
+      # `ActiveSupport::Dependencies.autoload_paths`.
+      #
+      # @return [Array<String>]
+      def all_autoload_paths
+        @all_autoload_paths ||= (autoload_once_paths + autoload_paths).uniq
+      end
+
+      # {#relative_autoload_once_paths} converted to absolute paths.
+      #
+      # @return [Array<String>]
+      def autoload_once_paths
+        @autoload_once_paths ||= relative_autoload_once_paths.collect { |relative_autoload_path|
+          root.join(relative_autoload_path).to_path
+        }
+      end
+
+      # {#relative_autoload_paths} converted to absolute paths.
+      #
+      # @return [Array<String>]
+      def autoload_paths
+        @autoload_paths ||= relative_autoload_paths.collect { |relative_autoload_once_path|
+          root.join(relative_autoload_once_path).to_path
+        }
+      end
+
+      # Paths relative {Metasploit::Model.root} that are to be added to `ActiveSupport::Dependencies.autoload_paths` and
+      # `ActiveSupport::Dependencies.autoload_once_paths`.
+      def relative_autoload_once_paths
+        @relative_autoload_once_paths ||= [
+            'lib'
+        ]
+      end
+
+      # Paths relative to {Metasploit::Model.root} that are to be added to `ActiveSupport::Dependencies.autoload_paths`,
+      # but not `ActiveSupport::Dependencies.autoload_once_paths`
+      #
+      # @return [Array<String>]
+      def relative_autoload_paths
+        @relative_autoload_paths ||= [
+            # Have to use ::File as File resolves to Metasploit::Model::File.
+            ::File.join('app', 'models'),
+            ::File.join('app', 'validators')
+        ]
+      end
+
+      # Adds {#autoload_paths} to `ActiveSupport::Dependencies.autoload_paths` if they are not already there.
+      #
+      # @return [void]
+      def set_autoload_paths
+        all_autoload_paths.each do |autoload_path|
+          unless ActiveSupport::Dependencies.autoload_paths.include? autoload_path
+            ActiveSupport::Dependencies.autoload_paths << autoload_path
+          end
+        end
+
+        autoload_once_paths.each do |autoload_once_path|
+          unless ActiveSupport::Dependencies.autoload_once_paths.include? autoload_once_path
+            ActiveSupport::Dependencies.autoload_once_paths << autoload_once_path
+          end
+        end
+      end
+    end
+  end
+end
