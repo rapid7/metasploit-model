956578f4fb4e703457c64eee2fbcf466964259da
Luke Imhoff <luke_imhoff@rapid7.com>
2013-08-29 09:59:36 -0500

    Lift Authory::SEED_ATTRIBUTES to Metasploit::Model
    [#56005168]
    
    Ensure that in-database seeds for `Mdm::Authority` and in-memory seeds
    for `Metasploit::Framework::Authority` (and `Dummy::Authority` for
    metasploit-model specs) are the same.

--- lib/metasploit/model/authority.rb
+++ lib/metasploit/model/authority.rb
@@ -4,6 +4,75 @@ module Metasploit
     module Authority
       extend ActiveSupport::Concern
 
+      #
+      # CONSTANTS
+      #
+
+      # Attributes for seeds.  Ensures that in-database seeds for `Mdm::Authority` and in-memory seeds for
+      # `Metasploit::Framework::Authority` are the same.
+      SEED_ATTRIBUTES = [
+          {
+              :abbreviation => 'BID',
+              :obsolete => false,
+              :summary => 'BuqTraq ID',
+              :url => 'http://www.securityfocus.com/bid'
+          },
+          {
+              :abbreviation => 'CVE',
+              :obsolete => false,
+              :summary => 'Common Vulnerabilities and Exposures',
+              :url => 'http://cvedetails.com'
+          },
+          {
+              :abbreviation => 'CWE',
+              :obsolete => false,
+              :summary => 'Common Weakness Enumeration',
+              :url => 'https://cwe.mitre.org/data/index.html'
+          },
+          {
+              :abbreviation => 'MIL',
+              :obsolete => true,
+              :summary => 'milw0rm',
+              :url => 'https://en.wikipedia.org/wiki/Milw0rm'
+          },
+          {
+              :abbreviation => 'MSB',
+              :obsolete => false,
+              :summary => 'Microsoft Security Bulletin',
+              :url => 'http://www.microsoft.com/technet/security/bulletin'
+          },
+          {
+              :abbreviation => 'OSVDB',
+              :obsolete => false,
+              :summary => 'Open Sourced Vulnerability Database',
+              :url => 'http://osvdb.org'
+          },
+          {
+              :abbreviation => 'PMASA',
+              :obsolete => false,
+              :summary => 'phpMyAdmin Security Announcement',
+              :url => 'http://www.phpmyadmin.net/home_page/security/'
+          },
+          {
+              :abbreviation => 'SECUNIA',
+              :obsolete => false,
+              :summary => 'Secunia',
+              :url => 'https://secunia.com/advisories'
+          },
+          {
+              :abbreviation => 'US-CERT-VU',
+              :obsolete => false,
+              :summary => 'United States Computer Emergency Readiness Team Vulnerability Notes Database',
+              :url => 'http://www.kb.cert.org/vuls'
+          },
+          {
+              :abbreviation => 'waraxe',
+              :obsolete => false,
+              :summary => 'Waraxe Advisories',
+              :url => 'http://www.waraxe.us/content-cat-1.html'
+          }
+      ]
+
       included do
         include ActiveModel::MassAssignmentSecurity
         include ActiveModel::Validations
