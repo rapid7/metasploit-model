bc2eec5948c01cc5287b632db0c3036c394d4f12
Luke Imhoff <luke_imhoff@rapid7.com>
2013-09-04 18:18:22 -0500

    Fix YARD
    [#56005352]

--- lib/metasploit/model/real_pathname.rb
+++ lib/metasploit/model/real_pathname.rb
@@ -1,10 +1,11 @@
 module Metasploit
   module Model
+    # Adds {#real_pathname} to class, which will convert `#real_path` `String` to a `Pathname`.
     module RealPathname
-      # {#real_path} as a `Pathname`.
+      # `#real_path` as a `Pathname`.
       #
-      # @return [Pathname] unless {#real_path} is `nil`.
-      # @return [nil] if {#real_path} is `nil`.
+      # @return [Pathname] unless `#real_path` is `nil`.
+      # @return [nil] if `#real_path` is `nil`.
       def real_pathname
         if real_path
           Pathname.new(real_path)
