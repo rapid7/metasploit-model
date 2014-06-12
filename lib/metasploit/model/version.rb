917f325c211bba0712455091e8ecbe6f088c01be
Luke Imhoff <luke_imhoff@rapid7.com>
2014-06-12 10:18:44 -0500

    Update version.rb and version_spec.rb for semantic versioning 2.0
    MSP-10011

--- lib/metasploit/model/version.rb
+++ lib/metasploit/model/version.rb
@@ -1,9 +1,33 @@
 module Metasploit
   module Model
-    # Metasploit::Model follows the {http://semver.org/  Semantic Versioning Specification}.  At this time, the API is
-    # considered unstable because certain code may not be shared between metasploit_data_models, metasploit-framework,
-    # and pro, so support code for that may be removed in the future.  Because of the unstable API the version should
-    # remain below 1.0.0
-    VERSION = '0.24.0'
+    # Holds components of {VERSION} as defined by {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0}.
+    module Version
+      # The major version number.
+      MAJOR = 0
+      # The minor version number, scoped to the {MAJOR} version number.
+      MINOR = 24
+      # The patch number, scoped to the {MINOR} version number.
+      PATCH = 1
+      # The prerelease name of the given {MAJOR}.{MINOR}.{PATCH} version number. Will not be defined on master.
+      PRERELEASE = 'semantic-versioning-2-0'
+
+      # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
+      # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
+      #
+      # @return [String] '{MAJOR}.{MINOR}.{PATCH}' on master.  '{MAJOR}.{MINOR}.{PATCH}-{PRERELEASE}' on any branch
+      #   other than master.
+      def self.full
+        version = "#{MAJOR}.#{MINOR}.#{PATCH}"
+
+        if defined? PRERELEASE
+          version = "#{version}-#{PRERELEASE}"
+        end
+
+        version
+      end
+    end
+
+    # @see Version.full
+    VERSION = Version.full
   end
 end
