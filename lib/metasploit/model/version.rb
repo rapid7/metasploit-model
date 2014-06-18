779eb1fb4d04903c3bd80a29de037df5ef2ec6d2
Luke Imhoff <luke_imhoff@rapid7.com>
2014-06-18 15:38:16 -0500

    Increment PATCH for compatible changes
    MSP-10039

--- lib/metasploit/model/version.rb
+++ lib/metasploit/model/version.rb
@@ -7,7 +7,7 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 24
       # The patch number, scoped to the {MINOR} version number.
-      PATCH = 1
+      PATCH = 2
       # The prerelease version, scoped to the {PATCH} version number.
       PRERELEASE = 'readme'
 
