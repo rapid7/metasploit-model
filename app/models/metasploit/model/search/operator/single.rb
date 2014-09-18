0eb152517a5138fd5ed1e031e5b5ae4f174a6b15
Luke Imhoff <luke_imhoff@rapid7.com>
2014-09-18 11:21:18 -0500

    Fix double include bug
    MSP-11359

--- app/models/metasploit/model/search/operator/single.rb
+++ app/models/metasploit/model/search/operator/single.rb
@@ -1,4 +1,4 @@
-require 'metasploit/model/search/operator/single'
+require 'metasploit/model/search/operator'
 
 # If all you want do is customize the name and operation `Class` that your custom operator class returns from
 # `#operate_on`, then you can subclass {Metasploit::Model::Search::Operator::Single} instead of
