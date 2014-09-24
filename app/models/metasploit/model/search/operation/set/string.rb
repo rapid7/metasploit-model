cab16490f415a40617a8518c2891704101cf0111
Luke Imhoff <luke_imhoff@rapid7.com>
2014-09-23 21:30:55 -0500

    travis-ci debugging
    MSP-11385
    
    Print $LOAD_PATH on travis-ci to try and figure out problem.

--- app/models/metasploit/model/search/operation/set/string.rb
+++ app/models/metasploit/model/search/operation/set/string.rb
@@ -1,5 +1,10 @@
 # Search operation on an attribute that has a `Set<String>` for acceptable
 # {Metasploit::Model::Search::Operation::Base values}.
 class Metasploit::Model::Search::Operation::Set::String < Metasploit::Model::Search::Operation::Set
-  include Metasploit::Model::Search::Operation::Value::String
+  begin
+    include Metasploit::Model::Search::Operation::Value::String
+  rescue LoadError => error
+    warn "$LOAD_PATH =\n#{$LOAD_PATH.map{ |p| "  #{p}"}.join("\n")}"
+    raise error
+  end
 end
