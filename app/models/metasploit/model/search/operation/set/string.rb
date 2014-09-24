c7fe412187cb88f575a2b90bb949abb413eac3b0
Luke Imhoff <luke_imhoff@rapid7.com>
2014-09-24 08:37:47 -0500

    Fix capitalization of file
    MSP-11385
    
    Accidentally capitalized file name, which works on OSX, but not on
    case-sensitive systems like Linux.

--- app/models/metasploit/model/search/operation/set/string.rb
+++ app/models/metasploit/model/search/operation/set/string.rb
@@ -1,10 +1,5 @@
 # Search operation on an attribute that has a `Set<String>` for acceptable
 # {Metasploit::Model::Search::Operation::Base values}.
 class Metasploit::Model::Search::Operation::Set::String < Metasploit::Model::Search::Operation::Set
-  begin
-    include Metasploit::Model::Search::Operation::Value::String
-  rescue LoadError => error
-    warn "$LOAD_PATH =\n#{$LOAD_PATH.map{ |p| "  #{p}"}.join("\n")}"
-    raise error
-  end
+  include Metasploit::Model::Search::Operation::Value::String
 end
