64ba25f435409b5347890d5a6f43d23be3074532
Luke Imhoff <luke_imhoff@rapid7.com>
2014-12-17 11:40:23 -0600

    Update version for branch
    MSP-11141

--- lib/metasploit/model/version.rb
+++ lib/metasploit/model/version.rb
@@ -7,7 +7,9 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 28
       # The patch number, scoped to the {MINOR} version number.
-      PATCH = 0
+      PATCH = 1
+      # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
+      PRERELEASE = 'extract-cache-from-metasploit-model'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
