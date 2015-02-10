60a970e06e8fa1face05213da585666abb1f2e0c
Luke Imhoff <luke_imhoff@rapid7.com>
2015-02-10 10:12:32 -0600

    Matcher, validate_nilness_of, to check `nil: true` validation
    MSP-12130

---
+++ spec/support/matchers/validate_nilness_of.rb
@@ -0,0 +1,32 @@
+RSpec::Matchers.define :validate_nilness_of do |attribute|
+  description do
+    "require nil for #{attribute}"
+  end
+
+  failure_message_for_should do |instance|
+    "Expected errors to include 'must be nil' when #{attribute} is set to an arbitrary string"
+  end
+
+  failure_message_for_should_not do |instance|
+    "Expected errors not to include 'must be nil' when #{attribute} is set"
+  end
+
+  match do |instance|
+    writer = :"#{attribute}="
+    instance.send(writer, nil)
+    instance.valid?
+    allow_nil = instance.errors[attribute].empty?
+
+    empty = ''
+    instance.send(writer, empty)
+    instance.valid?
+    disallow_empty = instance.errors[attribute].include?('must be nil')
+
+    present = 'present'
+    instance.send(writer, present)
+    instance.valid?
+    disallow_present = instance.errors[attribute].include?('must be nil')
+
+    allow_nil && disallow_empty && disallow_present
+  end
+end
