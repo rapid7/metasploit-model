e766014496e18c1bac08ffd7507b48c6e7d506cf
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-16 09:20:34 -0500

    Remove own yard task
    MSP-11070
    
    lib/tasks defined tasks are inherited by all downstream Rails projects,
    so it leads to multiple actions for the `yard` tasks.

--- lib/metasploit/model/version.rb
+++ lib/metasploit/model/version.rb
@@ -5,9 +5,9 @@ module Metasploit
       # The major version number.
       MAJOR = 0
       # The minor version number, scoped to the {MAJOR} version number.
-      MINOR = 29
+      MINOR = 30
       # The patch number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 2
+      PATCH = 0
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
       PRERELEASE = 'metasploit-yard'
 
