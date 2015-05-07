3faa34dabf553869aeb5f0dfe1dd2c95262db1a5
Lance Sanchez <lance.sanchez@gmail.com>
2015-05-07 15:04:42 -0500

    Remove PRERELEASE

--- lib/metasploit/model/version.rb
+++ lib/metasploit/model/version.rb
@@ -12,8 +12,6 @@ module Metasploit
       MINOR = 30
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
       PATCH = 2
-      # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'metasploit-version'
 
       #
       # Module Methods
