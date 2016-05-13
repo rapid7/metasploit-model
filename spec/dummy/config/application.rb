08ef51dbc656df4e08467e12e3da0b3d0ae24969
David Maloney <DMaloney@rapid7.com>
2016-05-13 09:53:07 -0500

    fix application config for dummy app
    this was causing database.yml to be
    incorrectly required

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,7 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require 'active_record/railtie'
+require 'active_model/railtie'
+require 'action_controller/railtie'
 
 Bundler.require(*Rails.groups)
 
